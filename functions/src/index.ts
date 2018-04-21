e5d3b798ed4e59d9540ae2119bc34b3d39015990
Leonardo Saraiva <vyper@maneh.org>
2018-04-21 00:27:15 -0300

    Remove listener on firestore and add to bigquery syncronally

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,20 +3,15 @@ import * as admin     from 'firebase-admin';
 
 import oauth       from './modules/oauth';
 import webhooks    from './modules/webhooks';
-import createEvent from './modules/events';
 
 admin.initializeApp({
   credential: admin.credential.cert({
-    projectId: 'biketastic-x23',
+    projectId: process.env.GCLOUD_PROJECT,
     clientEmail: (functions.config().credential.client_email),
     privateKey: (functions.config().credential.private_key).replace(/\\n/g, '\n')
   }),
-  databaseURL: 'https://biketastic-x23.firebaseio.com'
+  databaseURL: `https://${process.env.GCLOUD_PROJECT}.firebaseio.com`
 });
 
-exports.webhooks = functions.https.onRequest(webhooks);
 exports.oauth    = functions.https.onRequest(oauth);
-
-exports.createEvent = functions.firestore
-  .document('events/{eventId}')
-  .onWrite(event => createEvent);
+exports.webhooks = functions.https.onRequest(webhooks);
--- functions/src/modules/webhooks.ts
+++ functions/src/modules/webhooks.ts
@@ -1,5 +1,9 @@
+import * as functions from 'firebase-functions';
 import * as admin     from 'firebase-admin';
 import * as express   from 'express';
+import * as BigQuery  from '@google-cloud/bigquery';
+import * as crypto    from 'crypto';
+import axios          from 'axios';
 
 const app = express();
 
@@ -17,6 +21,54 @@ app.post('/strava', async (req, res) => {
   if (event.object_type !== 'activity') {
     console.log('[FINISHED] Object type is not activity');
     return res.status(200).json({ success: true, bigquery: false });
+  }
+
+  const uid = `strava:${event.owner_id}`;
+  const user = await admin.firestore().collection('users').doc(uid).get();
+  const accessToken = user.data().accessToken;
+
+  const response = await axios.get(
+    `https://www.strava.com/api/v3/activities/${event.object_id}`,
+    { headers: { 'Authorization': `Bearer ${accessToken}` } }
+   );
+
+  if (response.data.start_latlng === null)
+    delete response.data['start_latlng'];
+  if (response.data.end_latlng === null)
+    delete response.data['end_latlng'];
+  if (response.data.photos.primary !== null) {
+    response.data.photos.primary.urls['s600'] = response.data.photos.primary.urls['600'];
+    delete response.data.photos.primary.urls['600'];
+    response.data.photos.primary.urls['s100'] = response.data.photos.primary.urls['100'];
+    delete response.data.photos.primary.urls['100'];
+  }
+  response.data.start_date       = response.data.start_date.replace('T', ' ').replace(/Z$/, '');
+  response.data.start_date_local = response.data.start_date_local.replace('T', ' ').replace(/Z$/, '');
+  for (const lap of response.data.laps) {
+    lap.start_date       = lap.start_date.replace('T', ' ').replace(/Z$/, '');
+    lap.start_date_local = lap.start_date_local.replace('T', ' ').replace(/Z$/, '');
+  }
+  for (const segment_effort of response.data.segment_efforts) {
+    segment_effort.start_date       = segment_effort.start_date.replace('T', ' ').replace(/Z$/, '');
+    segment_effort.start_date_local = segment_effort.start_date_local.replace('T', ' ').replace(/Z$/, '');
+  }
+
+  const config = functions.config();
+  const datasetName = config.bigquery.dataset;
+  const tableName = config.bigquery.table;
+  const bigquery = new BigQuery();
+
+  try {
+    const dataset = await bigquery.dataset(datasetName);
+    const table = await dataset.table(tableName);
+    const row = {
+      insertId: crypto.randomBytes(20).toString('hex'),
+      json: response.data
+    };
+    await table.insert(row, { raw: true });
+  } catch (err) {
+    console.error(`[BIGQUERY] ${JSON.stringify(err)}`);
+    return res.status(500).json({ success: false, message: err })
   }
 
   return res.status(200).json({ success: true });
